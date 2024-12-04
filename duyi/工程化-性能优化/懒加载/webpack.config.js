2db15a4632096060f6993b8882e5b168a3c216bf
xulk <690978247@qq.com>
2024-12-04 18:38:11 +0800

    feat: 懒加载

---
+++ duyi/工程化-性能优化/懒加载/webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  output: {
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+  ]
+};
+
